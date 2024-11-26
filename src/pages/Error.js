590961f47e4863351725eddef83c9613de674de9
amiro hatami <amirohatami@amiros-Laptop.local>
2024-11-26 15:34:12 +0330

    Add explanation to App.js about the Outlet combo and the children form of routing to shoung different componnet, and adding Errorelement to the Rootlayout for poping out the error if somthibng happend

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,15 +1,14 @@
 import MainNavigation from "../components/MainNavigation";
 
 function ErroPage() {
-    return (
-        <>
-        <MainNavigation/>
-        <main>
-            <h1>An error occured!</h1>
-            <p>Could not find this page</p>
-        </main>
-
-        </>
-    )
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <h1>An error occured!</h1>
+        <p>Could not find this page</p>
+      </main>
+    </>
+  );
 }
-export default ErroPage
+export default ErroPage;
